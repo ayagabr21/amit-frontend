8b20d67ce5c8af67f6e853c96ec17147ae54cf0c
ayagabr21 <aya.gabr2000@gmail.com>
2021-11-01 21:21:34 +0200

    amit frontend project

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function()
+
+{
+    
+    // make the first item appear by defult 
+    // $(".accordian:first").css("display","block");
+
+    // // make the other items appear on click
+    // $("h6").click (function(){
+    //     $(".header span").removeClass("fa-minus").addClass("fa-plus")
+    //     $(this).children("span").removeClass("fa-plus").addClass("fa-minus")
+    //     $(this).next().slideToggle(500)
+
+    //     // make - change to +   
+    // })
+   
+    // $('#faq').accordion({heightStyle: 'panel'});
+
+
+    // scroll top btn
+    // make the btn fade in when it reach 1000 and disappear before that
+    // consol.log(1000)
+    $(window).scroll(function(){
+        if($(this).scrollTop()>1000){
+            $(".top").fadeIn(150);
+            
+        }
+        else{
+            $(".top").fadeOut(150);
+        }
+    })
+    // make the scrolling up slower
+    // $(".top").click(function(){
+
+    //     $(".body").animate({scrollTop:0},150)
+        
+    // })
+
+})
